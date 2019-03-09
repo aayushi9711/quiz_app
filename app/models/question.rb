3fe2c35e8e0a3f86d900cb9e563eaf2a255c0514
aayushi kumar <aayushi.1rn15ec002@gmail.com>
2019-03-09 21:30:22 +0530

    quiz_app

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   validates_uniqueness_of :description
   validates_presence_of :answer,:description
-  validates :description, length: { minimum: 2 , maximum:72}
+  #validates :description, length: { minimum: 2 , maximum:72}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 question=Question.new
-question.description="what is capital of tamil nadu?"
-question.answer="chennai"
-question.save
-question=Question.create([{description:'what is capital of Kerala?', answer:'trivandrum'},{description:'what is capital of punjab?', answer:'chandigarh'}])
-question=Question.new(description:"what is capital of J&k",answer:"srinagar")
-question.save
+#question.description="what is capital of tamil nadu?"
+#question.answer="chennai"
+#question.save
+#question=Question.create([{description:'what is capital of Kerala?', answer:'trivandrum'},{description:'what is capital of punjab?', answer:'chandigarh'}])
+#question=Question.new(description:"what is capital of J&k",answer:"srinagar")
+#question.save
+Question.create!(description: 'Is Ruby an Object Oriented Language', answer: 'yes')
+Question.create!(description: 'What is naming convention for file names?', answer: 'snakecase')
+Question.create!(description: 'What is rails?', answer: 'a framework')
+Question.create!(description: 'What is REPL for Ruby language ?', answer: 'irb')
+Question.create!(description: 'Rail models are typically based on what design pattern? ', answer: 'ActiveRecord')
+Question.create!(description: 'Which HTTP verb is used to delete model objects in a Rails application', answer: 'DELETE')
+Question.create!(description: 'What is the default port on which the rails servers starts up?', answer: '3000')
